ddbd52be71bc6ccd94f76027a183e306625717ac
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-02-11 12:24:42 -0800

    update

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args) {
+            var name = ClassLibrary1.Class1.GetName();
+            System.Console.WriteLine(string.Format("Name: {0}"),name);
+
+        }
+    }
+}
